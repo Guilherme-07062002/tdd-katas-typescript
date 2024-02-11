7c8eddb33fae1ac3d4d13f6a01108d8ac45fdb85
Guilherme-07062002 <guilherme762002@gmail.com>
2024-02-11 13:41:31 -0300

    refactor: remove add all number old solution

--- src/add-all-numbers/solutions/forof.add-all-numbers.ts
+++
@@ -1,9 +0,0 @@
-export function getTotal(numbers: number[]) {
-  let total = 0
-
-  for (const number of numbers) {
-    total += number
-  }
-
-  return total
-}
--- src/add-all-numbers/solutions/reduce.add-all-numbers.ts
+++
@@ -1,3 +0,0 @@
-export function getTotal(given: number[]) {
-  return given.reduce((sum, number) => sum + number, 0)
-}
